fdfd9b7a838ab3a8f10b26e2d90eec7b687e7626
Paul Bernitz <paulbernitz@web.de>
2018-07-06 00:48:43 +0200

    Desperately trying to fix the local  reloading in DOM.

--- tiny_template-0.2.js
+++ tiny_template-0.2.js
@@ -25,10 +25,12 @@ class TinyTemplate{
     splitAtJs.forEach((statement, index, arr) => {
       if(index===0) return;
       let expression = statement.substr(0, statement.indexOf(')'));
+      
       let evaluateInContext = (function(){
         return eval(expression);
       }).bind(this);
       let evaluated = evaluateInContext();
+
       splitAtJs[index] = evaluated + statement.substr(statement.indexOf(')') + 1);
     });
     raw = splitAtJs.join(''); 
@@ -57,7 +59,62 @@ class TinyTemplate{
 
     // Write changes to the DOM.
     let currentDomNode = document.getElementById(this.id);
-    currentDomNode.parentNode.replaceChild(newDomNode, currentDomNode);
+
+    // new
+    let changedOld = [];
+    let changedNew = [];
+    let allNew = newDomNode.getElementsByTagName('*');
+    let allOld = currentDomNode.getElementsByTagName('*');
+    
+    // Save all nodes that are in both the current and the new dom.
+    let stayingNodes = [];
+    for(let i=0; allOld[i] !== undefined; ++i){
+      for(let j=0; allNew[j] !== undefined; ++j){
+        if(allOld[i].isEqualNode(allNew[j])){
+          stayingNodes.push(allOld[i]);
+        }
+      }
+    }
+    console.log("Found matches:");
+    console.log(stayingNodes);
+
+    for(let i=0; allNew[i] !== undefined; ++i){
+      let oldNode = allOld[i];
+      let newNode = allNew[i];
+      if(oldNode===undefined){
+      
+      }else if(oldNode.isEqualNode(newNode) === false){
+        console.log("Change detected:");
+        console.log(oldNode);
+        console.log(newNode);
+        console.log("-------------------");
+
+        // @Todo: check following nodes for new matching ones.
+        // 1) Replace oldNode with newNode in DOM.
+        // 2) Also insert the following nodes form newNode's if they are not in the stayingNodes list into dom behind newNode.
+        // 3) Remove the following nodes from oldNode from DOM until it is one of the styling Nodes.
+        // 4) Continue loop
+        // The following should be removed, instead DOM gets directly changed above.
+        changedOld.push(oldNode);
+        changedNew.push(newNode);
+      }
+    }
+    console.log(changedOld);
+    console.log(changedNew);
+    
+    // Todo remove this eventually. (Besides the initial settings for first run)
+    // Read more above.
+    if(currentDomNode.childElementCount == 0)
+      currentDomNode.parentNode.replaceChild(newDomNode, currentDomNode);
+    else{
+      for(let i=0; i<changedOld.length; ++i){
+        currentDomNode.replaceChild(changedNew[i], changedOld[i]);
+      }
+    }
+    // new
+
+    // Uncomment for previous try.
+    //currentDomNode.parentNode.replaceChild(newDomNode, currentDomNode);
   }
 }
 TinyTemplate.IdCounter = 0;
