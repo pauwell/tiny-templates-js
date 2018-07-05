92a68b27bd98614e2189c4e552760a41fdf10cc4
Paul Bernitz <paulbernitz@web.de>
2018-07-06 00:48:02 +0200

    Saving the properties that got changed on last parse.

--- tiny_state.js
+++ tiny_state.js
@@ -4,6 +4,7 @@ class TinyState{
   constructor(template, state){
     this.template = template; // Reference to the parent template.
     this.state = state;       // State is an object with key-value pairs.
+    this.lastChangedProperties = []; // Contains the properties that got changed. Gets emptied after parsing.
 
     console.log(`TinyState constructed for ${template.name}!`);
   }
@@ -14,10 +15,12 @@ class TinyState{
       if(this.state.hasOwnProperty(prop) && this.state[prop] != newState[prop]){
         itHasChanged = true;
         this.state[prop] = newState[prop]; // Apply changes to state.
+        this.lastChangedProperties.push(prop);
       } 
     }
      if(itHasChanged){
        this.template.parseView(); // Reparse the template if the state was changed.
+       this.lastChangedProperties = []; // Reset after parsing.
      }
   }
 }
