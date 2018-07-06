10e3aeb3f9eab506c09da4a91836dffa287882b1
Paul Bernitz <paulbernitz@web.de>
2018-07-07 01:16:52 +0200

    Livereloading basically works now with :js. :if is somehow not detected by the current algo.

--- tiny_template-0.2.js
+++ tiny_template-0.2.js
@@ -20,42 +20,74 @@ class TinyTemplate{
   parseView(){
     let raw = this.textView;  // Do not change this.textView itself.
 
+    // Statement ID's are the same every time so they are used for detecting changes to the current state.
+    let updateIds = [];
+    console.log(this.state.lastChangedProperties);
     // :js
     let splitAtJs = raw.split(':js(');
     splitAtJs.forEach((statement, index, arr) => {
       if(index===0) return;
-      let expression = statement.substr(0, statement.indexOf(')'));
       
+      // Evaluate the expression in the context of this template.
+      let expression = statement.substr(0, statement.indexOf(')'));
       let evaluateInContext = (function(){
         return eval(expression);
       }).bind(this);
       let evaluated = evaluateInContext();
 
-      splitAtJs[index] = `<span id="js_${index}">${evaluated}</span>` + statement.substr(statement.indexOf(')') + 1);
+      // Create ID.
+      let id = `js_${index}`;
+
+      // If the expression contains a property that was changed it gets updated.
+      this.state.lastChangedProperties.forEach((prop)=>{
+        console.log("Exression: " + expression);
+        console.log("Prop: " + prop);
+        if(expression.indexOf(prop) !== -1 && updateIds.indexOf(id) === -1){
+          updateIds.push(id);
+          console.log("push js");
+          return;
+        }
+      });
+
+      splitAtJs[index] = `<span id="${id}">${evaluated}</span>` + statement.substr(statement.indexOf(')') + 1);
     });
-    raw = splitAtJs.join(''); 
+    raw = splitAtJs.join(''); // Write changes to raw.
 
     // :if
     let splitAtIf = raw.split(':if(');
     splitAtIf.forEach((statement, index, arr) => {
       if(index===0) return;
+    
+      // Evaluate the expression in the context of this template.
       let expression = statement.substr(0, statement.indexOf(')'));
       let evaluateInContext = (function(){
         return eval(expression);
       }).bind(this);
       let evaluated = evaluateInContext();
-      if(evaluated === false){
-        statement = `<span id="if_${index}">` + statement.substr(statement.indexOf(':fi'));
-      }else{
-        statement = `<span id="if_${index}">` + statement.substr(statement.indexOf(')') + 1);
-      }
-      console.log(statement);
+
+      // Create ID.
+      let id = `if_${index}`;
+
+      // If the expression contains a property that was changed it gets updated.
+      this.state.lastChangedProperties.forEach((prop)=>{
+        console.log("Exression: " + expression);
+        console.log("Prop: " + prop);
+        if(expression.indexOf(prop) !== -1 && updateIds.indexOf(id) === -1){
+            updateIds.push(id);
+            console.log("push if");
+            return;
+        }
+      });
+
+      // Remove content if statement evaluated to false.
+      statement = (evaluated === false) ?
+        `<span id="${id}">` + statement.substr(statement.indexOf(':fi'))
+      : `<span id="${id}">` + statement.substr(statement.indexOf(')') + 1);
+
       splitAtIf[index] = statement.replace(RegExp.escape(':fi'), '</span>');
     });
-    raw = splitAtIf.join('');
+    raw = splitAtIf.join(''); // Write changes to raw.
 
-
-    // new
     let activeDomNode = document.getElementById(this.id); // <div id="exmpl_1"></div>
 
     // Convert raw to html nodes and reattach id to root.
@@ -67,9 +99,28 @@ class TinyTemplate{
     let allNewNodes = convertedOutput.getElementsByTagName('*');
 
     // Append all children if they are not in the DOM currently.
-    //if(activeDomNode.childElementCount == 0)  
-    activeDomNode.parentNode.replaceChild(convertedOutput, activeDomNode);
+    if(activeDomNode.childElementCount == 0){  
+      activeDomNode.parentNode.replaceChild(convertedOutput, activeDomNode);
+    }else{
       
+      console.log('Allnewnodes:');
+      console.log(allNewNodes);
+      console.log(updateIds);
+      for(let i=0; allNewNodes[i] !== undefined; ++i){
+        console.log(updateIds);
+        if(updateIds.indexOf(allNewNodes[i].id) !== -1){
+
+          // @ Todo: Somehow here the js_ ids are detected but not the if_...
+
+          console.log("update " + allNewNodes[i].id);
+          let domRef = document.getElementById(allNewNodes[i].id);
+          domRef.parentNode.replaceChild(allNewNodes[i], domRef);
+        }
+      }
+
+      // @ TODO: check if the innerhtml of the node really changed before updateing. maybe its an if condition that it false the whhole
+        // time but gets updated the whole time because it just contained the value.
+    }
    // console.log("Active DOM node:");
    // console.log(activeDomNode.outerHTML);
    // console.log("Converted output:");
