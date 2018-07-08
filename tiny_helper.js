b6767c66e815b0ce63d7254e2c5d72041c086da3
Paul Bernitz <paulbernitz@web.de>
2018-07-08 04:58:00 +0200

    Added a helper function that finds closing braces.

--- tiny_helper.js
+++ tiny_helper.js
@@ -2,14 +2,31 @@
 
 /* Parse a string to a HTML collection.
   From: http://youmightnotneedjquery.com */
-  var parseHTML = function(str) {
-    var tmp = document.implementation.createHTMLDocument();
-    tmp.body.innerHTML = str;
-    return tmp.body.children; // Do NOT change it to 'childNodes' again, please!
-  };  
-  
-  /* Escape regex strings.
-    From: https://makandracards.com/makandra/15879-javascript-how-to-generate-a-regular-expression-from-a-string */
-  RegExp.escape = function(string) {
-    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
-  };
+var parseHTML = function(str) {
+  var tmp = document.implementation.createHTMLDocument();
+  tmp.body.innerHTML = str;
+  return tmp.body.children; // Do NOT change it to 'childNodes' again, please!
+};  
+
+/* Escape regex strings.
+  From: https://makandracards.com/makandra/15879-javascript-how-to-generate-a-regular-expression-from-a-string */
+RegExp.escape = function(string) {
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+};
+
+/* Find the index of the closing brace to the first occuring opening brace.
+*/
+let findClosingBraceIndex = function(statement){
+  if(statement.indexOf(')')< statement.indexOf('(')){
+    return statement.indexOf(')');
+  }else{
+    let bracesOpen = 1; 
+    for(let i=statement.indexOf('(');i<5000;++i){
+      if(statement[i] === '('){ ++bracesOpen; }
+      else if(statement[i] === ')'){ --bracesOpen; }
+      if(bracesOpen <= 0){ 
+        return i; 
+      }
+    }
+  }
+}
