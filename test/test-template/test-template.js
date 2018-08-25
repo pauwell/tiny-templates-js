70ae3ef127857759546f7d40c16c811d137611c2
Paul Bernitz <paulbernitz@web.de>
2018-08-25 22:52:48 +0200

    New syntax used.

--- test/test-template/test-template.js
+++ test/test-template/test-template.js
@@ -3,28 +3,41 @@
 // Test template.
 let myTemplate = new TinyTemplate(
   "my-template",
-  { name: "my-template", age: 99, counter: 0 },
+  {
+    name: "my-template",
+    age: 99,
+    counter: 0
+  },
+  {
+    increaseAge: function() {
+      this.changeState({ age: this.getState("age") + 1 });
+      console.log(this);
+    },
+    increaseCounter: function() {
+      this.changeState({ counter: this.getState("counter") + 1 });
+    }
+  },
   /*html*/ `
   <div id="my-template">
-    <button onclick="myTemplate.changeState({ 'age': myTemplate.getState('age') + 1 })">Increment age</button>
+    <button on-event="click" call="increaseAge">Increment age</button>
     <p id="lvl-1">[Lvl-1]</p> 
     <if expr="1==1">
       <div id="content">
           <if expr="1==1">
-            <p id="lvl-2a">[Lvl-2a] Name: {{ name }}</p>
+            <p id="lvl-2a">[Lvl-2a] Name: {{name}}</p>
           </if> 
           <if expr="2==2">
-            <p id="lvl-2b">[Lvl-2b] Age: {{ age }}</p> 
+            <p id="lvl-2b">[Lvl-2b] Age: {{age}}</p> 
           </if> 
       </div>
     </if>
-    <button onclick="myTemplate.changeState({'counter': myTemplate.getState('counter') + 1})">Update counter {{ counter }}</button>
-    <for var="j" from="0" to="this.getState('counter')" step="1">
+    <button on-event="click" call="increaseCounter">Update counter {{ counter }}</button>
+    <for var="j" from="0" to="{{counter}}" step="1">
       <hr>
       <b>Hello this is {{ j }}</b>
       <div>
-        <for var="i" from="this.getState('age')" to="this.getState('age') + 10" step="1">
-          <p>Brand new syntax with mustache: {{ name }}, {{j}},{{ i }}!!</p>
+        <for var="i" from="{{age}}" to="{{age}} + 10" step="1">
+          <p>Brand new syntax with mustache: {{name}}, {{ j }},{{ i }}!!</p>
         </for>
       </div>
     </for>
@@ -34,4 +47,4 @@ let myTemplate = new TinyTemplate(
   </div>`
 );
 
-myTemplate.updateView();
+myTemplate.mount(document.getElementById("app"));
